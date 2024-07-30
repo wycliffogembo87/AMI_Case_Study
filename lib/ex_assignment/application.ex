2871a970f855bd73fc63beb06b806649d74927e3
Wycliff Ogembo <wycliffogembo.dev@gmail.com>
2024-07-30 15:33:56 +0300

    Added an agent based key-value store & run mix format

--- lib/ex_assignment/application.ex
+++ lib/ex_assignment/application.ex
@@ -15,7 +15,9 @@ defmodule ExAssignment.Application do
       # Start the PubSub system
       {Phoenix.PubSub, name: ExAssignment.PubSub},
       # Start the Endpoint (http/https)
-      ExAssignmentWeb.Endpoint
+      ExAssignmentWeb.Endpoint,
+      # Start agent based key-value store
+      ExAssignment.KeyValueStore
       # Start a worker by calling: ExAssignment.Worker.start_link(arg)
       # {ExAssignment.Worker, arg}
     ]
---
+++ lib/ex_assignment/key_value_store.ex
@@ -0,0 +1,24 @@
+defmodule ExAssignment.KeyValueStore do
+    use Agent
+
+    @doc """
+    Starts a new key_value_store.
+    """
+    def start_link(_opts) do
+      Agent.start_link(fn -> %{} end, [name: __MODULE__])
+    end
+  
+    @doc """
+    Gets a value from the `key_value_store` by `key`.
+    """
+    def get(key) do
+      Agent.get(__MODULE__, &Map.get(&1, key))
+    end
+  
+    @doc """
+    Puts the `value` for the given `key` in the `key_value_store`.
+    """
+    def put(key, value) do
+      Agent.update(__MODULE__, &Map.put(&1, key, value))
+    end
+end
